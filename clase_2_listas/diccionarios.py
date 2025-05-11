1fa1aa85534d065e05fe2bd5494ab1aa671e6290
Daniel Matias Fernandez <daniel.mfernandez1999@gmail.com>
2025-05-10 22:02:17 -0300

    diccionarios

--- clase_2_listas/diccionarios.py
+++ clase_2_listas/diccionarios.py
@@ -4,6 +4,18 @@ persona1 = {
     'telefono': 2966,
     'sueldo': 15.000
     }
+persona2 = {
+    'nombre':"Pepe Lunes",
+    'apellido': "perez", 'edad': 14,
+    'telefono': 2966,
+    'sueldo': 15.000
+    }
+persona3 = {
+    'nombre':"Pepe Jueves",
+    'apellido': "perez", 'edad': 14,
+    'telefono': 2966,
+    'sueldo': 15.000
+    }
 # print(persona1['nombre'])
 
 # print(persona1['nombre'].split()[-1])
@@ -12,5 +24,9 @@ persona1 = {
 # print(persona1.values())
 # print(persona1.items())
 # print(len(persona1))
-key0, value0 = list(persona1.items())[0]
-print(key0, value0)
+# key0, value0 = list(persona1.items())[0]
+# print(key0, value0)
+
+listaDiccionarios = [persona1, persona2, persona3]
+print(listaDiccionarios)
+
